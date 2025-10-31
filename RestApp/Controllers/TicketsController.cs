27e8e4cdf7b894e0f48a84cec08c0c8a9d776280
Ether141 <42118307+Ether141@users.noreply.github.com>
2025-10-31 14:27:36 +0100

    feat: Add TicketsController and Ticket model

---
+++ RestApp/Controllers/TicketsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestApp.Models;
+using RestApp.Services;
+
+namespace RestApp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TicketsController : Controller
+{
+    private readonly ITicketsProvider _ticketsProvider;
+
+    public TicketsController(ITicketsProvider ticketsProvider)
+    {
+        _ticketsProvider = ticketsProvider;
+    }
+
+    [HttpGet]
+    public IActionResult GetTickets() => Ok(_ticketsProvider.GetAllTickets().Select(t => new { t.Id, t.Title }));
+
+    [HttpGet("{id}")]
+    public IActionResult GetTicket(int id)
+    {
+        Ticket? ticket = _ticketsProvider.GetTicket(id);
+
+        if (ticket == null)
+        {
+            var problem = new ProblemDetails
+            {
+                Title = "Ticket not found",
+                Detail = $"Ticket with id = {id} does not exist."
+            };
+
+            return NotFound(problem);
+        }
+
+        return Ok(ticket);
+    }
+
+    [HttpGet("assignee/{id}")]
+    public IActionResult GetTicketAssignee(int id)
+    {
+        var problem = new ProblemDetails
+        {
+            Title = "Unauthorized user",
+            Detail = "You do not have permissions to get information about assigned user to the ticket."
+        };
+
+        return Unauthorized(problem);
+    }
+}
---
+++ RestApp/Models/Ticket.cs
@@ -0,0 +1,9 @@
+﻿namespace RestApp.Models;
+
+public class Ticket
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public DateTime CreatedDate { get; set; }
+}
